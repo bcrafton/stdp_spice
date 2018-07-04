07bba33fd54154888c5b0a535e3b1c13e545f16e
Brian Crafton <crafton.b@husky.neu.edu>
2018-07-03 21:07:30 -0400

    A

--- indiveri1/fit1.m
+++ indiveri1/fit1.m
@@ -19,8 +19,10 @@ v_vso1 = evalsig(x, 'v_vo1');
 
 %%%%%%%%%%%%%%%%%%%%%%
 
-i_vsmem_fit = polyfitn([v_vsmem, v_vso1, i_is1], i_vsmem, 10);
-i_vso2_fit = polyfitn([v_vsmem, v_vso1, i_is1], i_vso2, 10);
+% i_vsmem_fit = polyfitn([v_vsmem, v_vso1, i_is1], i_vsmem, 10);
+i_vsmem_fit = fit([v_vsmem, v_vso1, i_is1], i_vsmem, 'linearinterp');
+% i_vso2_fit = polyfitn([v_vsmem, v_vso1, i_is1], i_vso2, 10);
+i_vso2_fit = fit([v_vsmem, v_vso1, i_is1], i_vso2, 'linearinterp');
 
 %%%%%%%%%%%%%%%%%%%%%%
 
@@ -52,13 +54,13 @@ for i = 1:steps
         iin = 0;
     end
     
-    dvdt = (1 / C1) * (polyvaln(i_vsmem_fit, [vmem, vo2, iin]));
+    % dvdt = (1 / C1) * (polyvaln(i_vsmem_fit, [vmem, vo2, iin]));
+    dvdt = (1 / C1) * (i_vsmem_fit([vmem, vo2, iin]));
     vmem = vmem + dvdt * dt;
     vmem = min(max(vmem, -0.1), 1);
-    
-    % disp(dvdt);
-    
-    dvdt = (1 / C2) * (polyvaln(i_vso2_fit, [vmem, vo2, iin]));
+
+    % dvdt = (1 / C2) * (polyvaln(i_vso2_fit, [vmem, vo2, iin]));
+    dvdt = (1 / C2) * (i_vso2_fit([vmem, vo2, iin]));
     vo2 = vo2 + dvdt * dt;
     vo2 = min(max(vo2, -0.1), 1.0);
     
